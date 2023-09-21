e1e2df4ada9f2e58c8efec672938e1edff19b42a
clferma1 <clferma1@i3b.upv.es>
2023-09-21 15:24:30 +0200

    Created MyDatasetCustom and created DataGeneratorMCEPS for preloading the data in RAM memory.

--- data_loader.py
+++ data_loader.py
@@ -34,8 +34,55 @@ def to_categorical(y, num_classes=None):
     categorical = np.reshape(categorical, output_shape)
     return categorical
 
-
 class MyDataset(data.Dataset):
+    """Dataset for MCEP features and speaker labels."""
+
+    def __init__(self, speakers_using, data_dir):
+        self.speakers = speakers_using
+        self.spk2idx = dict(zip(self.speakers, range(len(self.speakers))))
+        self.prefix_length = len(self.speakers[0])
+
+        mc_files = glob.glob(join(data_dir, '*.npy'))
+        mc_files = [i for i in mc_files if basename(i)[:self.prefix_length] in self.speakers]
+        self.mc_files = self.rm_too_short_utt(mc_files)
+        self.num_files = len(self.mc_files)
+        print("\t Number of training samples: ", self.num_files)
+        for f in self.mc_files:
+            mc = np.load(f)
+            if mc.shape[0] <= min_length:
+                print(f)
+                raise RuntimeError(
+                    f"The data may be corrupted! We need all MCEP features having more than {min_length} frames!")
+
+    def rm_too_short_utt(self, mc_files, min_length=min_length):
+        new_mc_files = []
+        for mc_file in mc_files:
+            mc = np.load(mc_file)
+            if mc.shape[0] > min_length:
+                new_mc_files.append(mc_file)
+        return new_mc_files
+
+    def sample_seg(self, feat, sample_len=min_length):
+        assert feat.shape[0] - sample_len >= 0
+        s = np.random.randint(0, feat.shape[0] - sample_len + 1)
+        return feat[s:s + sample_len, :]
+
+    def __len__(self):
+        return self.num_files
+
+    def __getitem__(self, index):
+        filename = self.mc_files[index]
+        spk = basename(filename).split('_')[0]
+        spk_idx = self.spk2idx[spk]
+        mc = np.load(filename)
+        mc = self.sample_seg(mc)
+        mc = np.transpose(mc, (1, 0))  # (T, D) -> (D, T), since pytorch need feature having shape
+        # to one-hot
+        spk_cat = np.squeeze(to_categorical([spk_idx], num_classes=len(self.speakers)))
+
+        return torch.FloatTensor(mc), torch.LongTensor([spk_idx]).squeeze_(), torch.FloatTensor(spk_cat)
+
+class MyDatasetPreloaded(data.Dataset):
     """Dataset for MCEP features and speaker labels."""
 
     def __init__(self, speakers_using, data_dir):
@@ -153,19 +200,21 @@ class TestDataset(object):
         return batch_data
 
 
-def get_loader(speakers_using, data_dir, batch_size=32, mode='train', num_workers=1):
-    dataset = MyDataset(speakers_using, data_dir)
-    # data_loader = data.DataLoader(dataset=dataset,
-    #                               batch_size=batch_size,
-    #                               shuffle=(mode == 'train'),
-    #                               num_workers=num_workers,
-    #                               drop_last=True)
+def get_loader(speakers_using, data_dir, batch_size=32, mode='train', num_workers=1, preload_data=False):
 
-    data_generator = DataGeneratorMCEPS(dataset=dataset,
-                                        batch_size=batch_size,
-                                        shuffle=True,
-                                        num_workers=num_workers)
-
+    if preload_data:
+        dataset = MyDatasetPreloaded(speakers_using, data_dir)
+        data_generator = DataGeneratorMCEPS(dataset=dataset,
+                                            batch_size=batch_size,
+                                            shuffle=(mode == 'train'),
+                                            num_workers=num_workers)
+    else:
+        dataset = MyDataset(speakers_using, data_dir)
+        data_generator = data.DataLoader(dataset=dataset,
+                                      batch_size=batch_size,
+                                      shuffle=(mode == 'train'),
+                                      num_workers=num_workers,
+                                      drop_last=True)
     return data_generator
 
 
@@ -193,7 +242,7 @@ class DataGeneratorMCEPS(object):
         # If dataset is completed, stop the Iterator
         if self._idx >= len(self.dataset.mc_files_loaded):
             self._reset()
-            raise StopIteration()
+            # raise StopIteration()
 
         # Determine the indices for the current batch
         start_idx = self._idx
