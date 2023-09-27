c0121b24d6fd8d4116fe73a3afae720fb40b28eb
clferma1 <clferma1@i3b.upv.es>
2023-09-27 12:02:47 +0200

    Log_step default value to 10

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ if __name__ == '__main__':
                         default= ['p262', 'p272', 'p229', 'p232', 'p292', 'p293', 'p360', 'p361', 'p248', 'p251'])
 
     # Step size.
-    parser.add_argument('--log_step', type=int, default=1) #10
+    parser.add_argument('--log_step', type=int, default=10) #10
     parser.add_argument('--sample_step', type=int, default=10000) #10000
     parser.add_argument('--model_save_step', type=int, default=10000) #10000
     parser.add_argument('--lr_update_step', type=int, default=1000)
