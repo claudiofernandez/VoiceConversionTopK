821c5c4a394e7c12edcde2fbed8a7f35c38f53a5
clferma1 <clferma1@i3b.upv.es>
2023-10-08 17:28:20 +0200

    Lr_update_step=10000

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ if __name__ == '__main__':
     parser.add_argument('--log_step', type=int, default=100) #10
     parser.add_argument('--sample_step', type=int, default=10000) #10000
     parser.add_argument('--model_save_step', type=int, default=10000) #10000
-    parser.add_argument('--lr_update_step', type=int, default=1000)
+    parser.add_argument('--lr_update_step', type=int, default=10000)
 
     config = parser.parse_args()
 
