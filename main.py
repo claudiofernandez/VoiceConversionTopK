feab678d3c012c1f41e0b2c7cffa0fc76e8f8bde
clferma1 <clferma1@i3b.upv.es>
2023-10-11 17:26:36 +0200

    Added set_seed function for reproducibility

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import os
 import argparse
 from solver import Solver, SolverCustom
 from data_loader import get_loader, TestDataset
-from torch.backends import cudnn
+import torch
 import mlflow
+import numpy as np
+import random
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 def str2bool(v):
     return v.lower() in 'true'
 
 
 def main(config):
-    # For fast training.
-    cudnn.benchmark = True
+    # Set seeds for reproducibility
+    set_seed(45)
 
     print("Directories:", os.listdir(os.getcwd()))
     # Create needed directories
